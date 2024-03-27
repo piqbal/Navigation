486039f48ae5e932566a6f85ea66f27f251388c8
Pervaiz Iqbal <piqbal325@gmail.com>
2024-03-27 23:55:02 +0500

    first commit

---
+++ src/app/About/page.tsx
@@ -0,0 +1,12 @@
+"client side"
+import Link from "next/link";
+
+export default function About(){
+    return(
+        <main>
+            <h1><b>About Page</b></h1>
+
+            <Link href="/"> Go to HomePage</Link>
+        </main>
+    )
+}
---
+++ src/app/Login/page.tsx
@@ -0,0 +1,15 @@
+"client side"
+import Link from "next/link"
+
+export default function Login(){
+    return(
+        <main>
+            <div>
+            <h1><b>Login Page</b></h1>   
+
+            <Link href="/">Go to HomePage</Link> 
+            </div>
+            
+        </main>
+    )
+}
